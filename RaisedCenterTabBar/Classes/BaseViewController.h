772fbad7ee4b7ed83fddb30d1768abfe39bebb2b
Aakash <aakash09@gmail.com>
2011-09-20 12:06:04 -0400

    Added the ability to assign a target and action to the center bar when adding it.

--- RaisedCenterTabBar/Classes/BaseViewController.h
+++ RaisedCenterTabBar/Classes/BaseViewController.h
@@ -33,6 +33,6 @@
 -(UIViewController*) viewControllerWithTabTitle:(NSString*)title image:(UIImage*)image;
 
 // Create a custom UIButton and add it to the center of our tab bar
--(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage;
+-(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage target:(id)target action:(SEL)action;
 
 @end
